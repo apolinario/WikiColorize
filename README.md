335ea2cd4f21152cf868c5b333f731765bf86c22
apolinario <eu@apolinar.io>
2018-12-31 16:36:24 -0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# WikiColorize
+# WikiColorize bot
+
+##What
+This is the source code for a Python bot that pull random images from Wikipedia, colorize them with @algorithmiaio colorize machine learning algorithm, and post to Twitter ([@WikiColorize](https://twitter.com/WikiColorize "@WikiColorize"))
+
+## Why
+For fun. Deep learning is good at those kinds of tasks, Algorithmia has this Colorization Demo that can run without cost, only adding their watermark, Twitter is the right place for those kinds of bots, so the stars aligned.
+
+## How
+1. Get [WikiMedia random file URL ](http://commons.wikimedia.org/wiki/Special:Random/File "WikiMedia random file URL ") and return the main file and metadata via XPath using `lxml`
+2. Checks if the WikiMedia file is an image (there is audio, PDF and other file formats that do not matter for this application there) - if not, go back to step 1
+3. Checks if it is black and white using `Pillow` to check if the image is monochrome - if not, go back to step 1
+4. (Optional Step) Tries to identify if the monochrome image looks like a scanned document using `pytesseract` OCR, if it does, go back to step 1. This step is here because if `Tesseract` doesn't recognize the image as a document, it is more likely that it is a photo
+5. Saves the photo and inputs it to [Algorithmia Colorization API](https://demos.algorithmia.com/colorize-photos/ "Algorithmia Colorization Demo API"), that uses Deep Learning to try to guess the right colors for a B&W image, and returns the colorized photo
+6. Tweets the colorized photo (with metadata and the URL)
+
+### Limitations
+Trying to guess if the image is a photo/picture or a document is not optimal can be improved.
+
+The colorization API is far from perfect for most images. However it is great to see state of the art in the field of automatic colorization with real data.
---
+++ wikicolorize.py
@@ -0,0 +1,123 @@
+import base64
+from lxml import html
+import requests
+import Algorithmia
+from PIL import Image,ImageChops
+from io import BytesIO
+import pytesseract
+import tweepy
+
+#Gets a random wikipedia image and context
+def get_wikipedia_random(random_wiki_url):
+	wiki_image_data = requests.get(random_wiki_url)
+	wiki_image_site = html.fromstring(wiki_image_data.content)
+	#Gets the media file URL from WikiMedia
+	image_url = wiki_image_site.xpath('//div[@class="fullImageLink"]/a/img/@src')	
+	#Gets the image description, removing the "English" part
+	image_description = wiki_image_site.xpath('//div[contains(@class,\'description\') and contains(@lang,\'en\')]/span/../text()')
+	#Gets the redirected URL after WikiMedia's randomization
+	media_url = wiki_image_data.url
+	print(media_url)
+	#Gets the images categories (clearner than description most times, but less descriptive as well)
+	image_categories = wiki_image_site.xpath('//div[@class="mw-normal-catlinks"]/ul/li/a/text()') 
+	print(image_url)
+	#Checks if it is a JPG image
+	if image_url and image_url[0] is not None and image_url[0].lower().endswith(('.jpg', '.jpeg')):
+		wiki_image = requests.get(image_url[0])
+		byte_image = Image.open(BytesIO(wiki_image.content))
+		image_path = 'bw.jpg'
+		byte_image.save('/a/web/path/'+image_path,format="JPEG") #Here you have to save the file in a path that can be accessed from the web (because of line 52)
+		is_bw = is_bw_image(byte_image)
+		if is_bw:
+			if not is_document(byte_image):
+				colored_image = colorize_image(image_path)
+				if colored_image:
+					if tweet_image(colored_image,media_url,image_description,image_categories):
+						return True
+					else:
+						return False
+				else:
+					return False
+			else:
+				return False
+		else:
+			return False
+	else:
+		return False
+
+#Colorize Image 
+def colorize_image(image_path):
+	client = Algorithmia.client('YOUR-ALGORITHMIA-API-KEY')
+	algo = client.algo('algorithmiahq/ColorizationDemo/1.1.23') #If you pay for it you may use the non-demo Colorization API 
+	input_url = 'http://your-web-host'+image_path #I know this sucks, but it seems like Algorithmia only accepts a URL as input, not an image (image saved in line 29)
+	try:
+		colored_image = algo.pipe(input_url).result[1]
+		filename_colored = save_image(colored_image)
+		if filename_colored:
+			return filename_colored
+		else:
+			return False	
+	except:
+		return False
+	
+#Tries to OCR a scanned page with Tesseract OCR, if it fails, it probably isn't a document and is more likely to be a photo
+def is_document(image):
+	try:
+		print(pytesseract.image_to_data(image))
+		return True
+	except Exception as e:
+		return False
+
+#Checks if the image is black and white (Code by @Karl K @ https://stackoverflow.com/a/34175631/1284186)
+def is_bw_image(im):
+	"""
+	Check if image is monochrome (1 channel or 3 identical channels)
+	"""
+	if im.mode not in ("L", "RGB"):
+		return False
+		raise ValueError("Unsuported image mode")
+
+	if im.mode == "RGB":
+		rgb = im.split()
+		if ImageChops.difference(rgb[0],rgb[1]).getextrema()[1]!=0: 
+			return False
+		if ImageChops.difference(rgb[0],rgb[2]).getextrema()[1]!=0: 
+			return False
+	return True
+
+#Save the colored image in a *.png file
+def save_image(imgstring):
+	imgdata = base64.b64decode(imgstring)
+	filename = 'colored.png'  # I assume you have a way of picking unique filenames
+	try:
+		with open(filename, 'wb') as f:
+			f.write(imgdata)
+			f.close()
+		return filename
+	except:
+		return False
+
+#Twitter API bureaucracy
+def twitter_api():
+    access_token = 'YOUR-ACCESS-TOKEN'
+    access_token_secret = 'YOUR-ACCESS-TOKEN-SECRET'
+    consumer_key = 'YOUR-CONSUMER-KEY'
+    consumer_secret = 'YOUR-CONSUMER-SECRET'
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    return api
+
+#Tweets the image with the description and/or category
+def tweet_image(image, url, description, categories):
+	api = twitter_api()
+	api.update_with_media(image, status=categories[0]+' '+url)
+	return True
+
+random_wiki_url = 'http://commons.wikimedia.org/wiki/Special:Random/File'
+get_wikipedia_random(random_wiki_url)
+while True:
+  converted = get_wikipedia_random(random_wiki_url)
+  if converted:
+    break
